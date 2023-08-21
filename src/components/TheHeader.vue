9ac6579b6c910744e146f6f16d36436f7c4a0a12
jfavlam <jfavlam@pm.me>
2023-08-21 11:18:52 +0200

    feat: Add header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,20 +1,24 @@
 <template>
   <nav class="bg-slate-600">
-    <h1 class="font-display font-bold text-3xl">{{ $t("general.title") }}</h1>
-    <p>{{ $t("general.headline") }}</p>
-    <div class="lang-dropdown">
-      <select v-model="selectedLanguage">
-        <option v-for="lang in languages" :key="lang.value" :value="lang.value">
-          {{ lang.label }}
-        </option>
-      </select>
+    <div class="container m-auto px-4 py-4">
+      <div class="flex justify-between">
+        <div class="text-white">
+          <h1 class="font-display font-bold text-2xl">
+            {{ $t("general.title") }}
+          </h1>
+          <p>{{ $t("general.headline") }}</p>
+        </div>
+
+        <LangPicker />
+      </div>
     </div>
   </nav>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
+import LangPicker from "../components/LangPicker.vue";
 
 const { locale } = useI18n();
 const selectedLanguage = ref(locale.value);
