9a6ee456984bbff39911702bcf7d4164eb7c53fa
jfavlam <jfavlam@pm.me>
2023-08-21 11:16:43 +0200

    feat: Configure all languages

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,19 @@ import "./style.css";
 import App from "./App.vue";
 import { createI18n } from "vue-i18n";
 
-import en from "./locales/en.json";
+import de from "./locales/de.json";
 import fr from "./locales/fr.json";
+import en from "./locales/en.json";
 
 const messages = {
-  en: en,
+  de: de,
   fr: fr,
+  en: en,
 };
 
 const i18n = createI18n({
   legacy: false,
-  locale: "en", // Default locale
+  locale: "de", // Default locale
   messages,
 });
 
